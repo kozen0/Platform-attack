4f44b1971ce46caea4ed8211206e55c4f17492a7
tamari tinsley <tamaritinsley16@gmail.com>
2018-12-12 17:57:57 -0800

    added bace files

---
+++ game.js
@@ -0,0 +1,12 @@
+var config = {
+    type:Phaser.AUTO,
+    width:800,
+    height:600,
+    physics: {
+        defoult:'arcade',
+        arcade: {
+            gravity: {Y 200}
+        }
+    },
+    scene: [Example1]
+};
